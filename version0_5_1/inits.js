a8d05094b92232741c2fdb5d26f07dfd54fec60b
Bernkastelf <r.e.d.a.c.t.e.d@yandex.ru>
2019-08-30 22:35:41 +0400

    counters fixed 4

--- version0_5_1/inits.js
+++ version0_5_1/inits.js
@@ -1,3 +1,4 @@
+var inited = false;
 function destroyElement(element, anim = true){
 	element=element.filter('.element').not('.static');//filter unkillable statics
 	element.droppable( "destroy" );
@@ -6,52 +7,57 @@ function destroyElement(element, anim = true){
 	else element.remove();
 	element.data("isDead", 1);
 }
-gameInit();
+
 function gameInit() {
-	destroyElement($('#board').children('.element'), false);
-	$("#vote_stats").hide();
-	$("#vote_result").hide();
-	$("#abyss").droppable({drop: function(e, ui){
-			destroyElement(ui.helper); 
-			refreshHint();	
-		}});
-	$("body").selectable({
-		filter:'#board .element', 
-		distance:2,
-		stop: onSelectStop,
-		cancel: '.ui-dialog, #stack, #info, #abyss, .element'
+	if(!inited) {
+		inited = true;
+		destroyElement($('#board').children('.element'), false);
+		$("#vote_stats").hide();
+		$("#vote_result").hide();
+		$("#abyss").droppable({drop: function(e, ui){q
+				destroyElement(ui.helper); 
+				refreshHint();	
+			}});
+		$("body").selectable({
+			filter:'#board .element', 
+			distance:2,
+			stop: onSelectStop,
+			cancel: '.ui-dialog, #stack, #info, #abyss, .element'
+			});
+		$('#stack-btn').hide();
+		$("#help").dialog({ autoOpen: false, position:'right', open: renderHint });
+		$("#element_hint").dialog({ autoOpen: false, width:320 });
+		$("#payment_dialog").dialog({ autoOpen: false, width: 'auto' }).bind('dialogclose', function() {getHintCount();});
+		$("#recipe_list").dialog({ autoOpen: false, position:'left'});
+		$("#err_msg").dialog({ autoOpen: false});
+		$("#info_msg").dialog({ autoOpen: false, width:500});
+		$("#welcome_dialog").dialog({ autoOpen: ($.cookie('welcomed')?false:true), width:800  });
+		$("#elementFilter").keyup(function(event) {
+			filterStack($("#elementFilter").val());
 		});
-	$('#stack-btn').hide();
-	$("#help").dialog({ autoOpen: false, position:'right', open: renderHint });
-	$("#element_hint").dialog({ autoOpen: false, width:320 });
-    $("#payment_dialog").dialog({ autoOpen: false, width: 'auto' }).bind('dialogclose', function() {getHintCount();});
-	$("#recipe_list").dialog({ autoOpen: false, position:'left'});
-	$("#err_msg").dialog({ autoOpen: false});
-	$("#info_msg").dialog({ autoOpen: false, width:500});
-	$("#welcome_dialog").dialog({ autoOpen: ($.cookie('welcomed')?false:true), width:800  });
-	$("#elementFilter").keyup(function(event) {
-		filterStack($("#elementFilter").val());
-	});
-    $("#showHelp").click(function() {
-        if ($('#help').dialog('isOpen')) $('#help').dialog('close');
-        else $('#help').dialog('open');
-        
-        reachGoal('SHOW_HINTS');
-    });
-	
-	applySettings(settings);
-	toggleSort($('#order').val());
-	
-	sortKeys(reactions);
-	var test1 = test();
-	var total = test1.total;
-	finals = test1.finals;
-	wrongs = test1.wrongs;
-	element_count = total.length;
-	refreshStat();
-	if(settings.debug == "true") console.log("Game Inited.");
-	//so.. we are ready, lets go
-	placeElements(react(inits, true),{top: $('#stack').offset().top+$('#stack').height()+200,
-                    left: $('body').width()/2-100}, true);
-    updateCounters();
+		$("#showHelp").click(function() {
+			if ($('#help').dialog('isOpen')) $('#help').dialog('close');
+			else $('#help').dialog('open');
+			
+			reachGoal('SHOW_HINTS');
+		});
+		
+		applySettings(settings);
+		toggleSort($('#order').val());
+		
+		sortKeys(reactions);
+		var test1 = test();
+		var total = test1.total;
+		finals = test1.finals;
+		wrongs = test1.wrongs;
+		element_count = total.length;
+		refreshStat();
+		if(settings.debug == "true") console.log("Game Inited.");
+		//so.. we are ready, lets go
+		placeElements(react(inits, true),{top: $('#stack').offset().top+$('#stack').height()+200,
+						left: $('body').width()/2-100}, true);
+		updateCounters();
+	}
 }
+
+gameInit();
--- versionControl.js
+++ versionControl.js
@@ -1,11 +1,11 @@
 if (settings.version) {
     var updates = {
         "0.5.1": [
-            "version0_5_1/inits.js",
             "version0_5_1/fantom.js",
             "version0_5_1/groupblock.js",
             "version0_5_1/saveload.js",
-            "version0_5_1/border.js"
+            "version0_5_1/border.js",
+            "version0_5_1/inits.js"
         ]
     }
 
@@ -14,7 +14,7 @@ if (settings.version) {
 
     function createScriptElement(src) {
         var script = document.createElement("script");
-        script.src = "https://gitcdn.xyz/cdn/denys00/alchdev/master/" + src;
+        script.src = "https://min.gitcdn.xyz/cdn/denys00/alchdev/master/" + src;
         return script;
     }
 
