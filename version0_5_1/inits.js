30855894d2b42870532ebd87952f74331eb5f79b
Bernkastelf <r.e.d.a.c.t.e.d@yandex.ru>
2019-08-30 21:49:26 +0400

    counters fixed 2

--- version0_5_1/inits.js
+++ version0_5_1/inits.js
@@ -1,4 +1,14 @@
+function destroyElement(element, anim = true){
+	element=element.filter('.element').not('.static');//filter unkillable statics
+	element.droppable( "destroy" );
+	//element.effect(destroy_effects[Math.floor(Math.random()*destroy_effects.length)],{},1000, function(){element.remove();})
+	if(anim) element.fadeOut(1000, function(){element.remove();});
+	else element.remove();
+	element.data("isDead", 1);
+}
+
 function gameInit(){
+	destroyElement($('#board').children('.element'), false);
 	$("#vote_stats").hide();
 	$("#vote_result").hide();
 	$("#abyss").droppable({drop: function(e, ui){
