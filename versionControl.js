b7e83796ba38a9c5427a7b2d0892b676b3b198fe
Denys Denysuik <denysuikdenys@example.com>
2019-08-30 15:38:17 +0300

    version control fix 3

--- versionControl.js
+++ versionControl.js
@@ -1,21 +1,23 @@
-var updates = {
-    "0.5.1": [
-        "version0_5_1/inits.js",
-        "version0_5_1/fantom.js",
-        "version0_5_1/groupblock.js",
-        "version0_5_1/saveload.js",
-        "version0_5_1/border.js"
-    ]
+if (settings.version) {
+    var updates = {
+        "0.5.1": [
+            "version0_5_1/inits.js",
+            "version0_5_1/fantom.js",
+            "version0_5_1/groupblock.js",
+            "version0_5_1/saveload.js",
+            "version0_5_1/border.js"
+        ]
+    }
+
+    var head = document.getElementsByTagName('head')[0];
+    var scripts = updates[settings.version];
+
+    function createScriptElement(src) {
+        var script = document.createElement("script");
+        script.src = "https://cdn.jsdelivr.net/gh/denys00/alchdev/" + src;
+        return script;
+    }
+
+    for (link of scripts)
+        head.appendChild(createScriptElement(link));
 }
-
-var head = document.getElementsByTagName('head')[0];
-var scripts = updates[settings.version];
-
-function createScriptElement(src) {
-    var script = document.createElement("script");
-    script.src = "https://cdn.jsdelivr.net/gh/denys00/alchdev/" + src;
-    return script;
-}
-
-for (link in scripts)
-    head.appendChild(createScriptElement(link));
