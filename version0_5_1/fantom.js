01969e465411bb32863175e4479ef98325354b72
Denys Denysuik <denysuikdenys@example.com>
2019-08-30 16:55:37 +0300

    fantom, groupblock, saveload fixed

---
+++ version0_5_1/fantom.js
@@ -0,0 +1,12 @@
+function destroyElement(elem) {
+    elem = elem.filter('.element').not('.static');
+
+    elem.draggable('disable');
+    elem.droppable('disable');
+
+    elem.fadeOut(1000, () => {
+        elem.remove();
+    });
+
+    elem.data("isDead", 1);
+};
---
+++ version0_5_1/saveload.js
@@ -0,0 +1,28 @@
+function getModId() {
+    var regex = /[^\d]*(\d+)[^\d]*/gm;
+    var res = regex.exec($('#load')[0].onclick.toString());
+    return res[1];
+}
+
+$('#save a')[0].onclick = () => {
+    $('#save').append('<span id="save_msg">(игра сохраняется)</span>');
+    stopGame();
+
+    setTimeout(() => {
+        runGame();
+        save('/versions/' + getModId() + '/save/');
+        $('#save_msg').remove();
+    }, 2000);
+}
+
+function stopGame() {
+    $('.element').draggable('disable');
+    $('.element').droppable('disable');
+    $('.element').selectable('disable');
+}
+
+function runGame() {
+    $('.element').draggable('enable');
+    $('.element').droppable('enable');
+    $('.element').selectable('enable');
+}
