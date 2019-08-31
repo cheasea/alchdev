d0d69f48eb602a01fec2a201bbef876795b926d4
Denys Denysuik <denysuikdenys@example.com>
2019-08-31 23:08:38 +0300

    saveload fixed

--- version0_5_1/saveload.js
+++ version0_5_1/saveload.js
@@ -1,28 +1,54 @@
-function getModId() {
-    var regex = /[^\d]*(\d+)[^\d]*/gm;
-    var res = regex.exec($('#load')[0].onclick.toString());
-    return res[1];
-}
+function save(to) {
+    var elements = [];
+    $('#board').children('.element').not(':data(toKill,1)').not(':data(maybeKill,1)').each(function () {
+        elements.push({
+            'name': $(this).data('elementName'),
+            'offset': $(this).offset()
+        });
+    });
+    var data = {
+        "opened": opened,
+        "recipes": recipes,
+        "elements": elements,
+        "counters": counters
+    };
 
-$('#save a')[0].onclick = () => {
-    $('#save').append('<span id="save_msg">(игра сохраняется)</span>');
-    stopGame();
+    var success = function () {
+        $('#save').fadeOut();
+    };
+    var error = function () {
+        alert("Не удалось сохранить игру.");
+    };
 
-    setTimeout(() => {
-        runGame();
-        save('/versions/' + getModId() + '/save/');
-        $('#save_msg').remove();
-    }, 2000);
-}
+    if (use_local_storage_to_save) {
+        try {
+            window.localStorage[local_mod_save_key] = $.toJSON(data);
+            success();
+        } catch (e) {
+            error();
+        }
+    } else {
+        var saveToServer = function () {
+            $.post(to, {
+                "data": $.toJSON(data)
+            }, function (data) {
+                if (data == 'OK') {
+                    success();
+                } else {
+                    error();
+                }
+            });
+        };
 
-function stopGame() {
-    $('.element').draggable('disable');
-    $('.element').droppable('disable');
-    $('body').selectable('disable');
-}
-
-function runGame() {
-    $('.element').draggable('enable');
-    $('.element').droppable('enable');
-    $('body').selectable('enable');
+        if (typeof (VK_api) != 'undefined') {
+            VK_StorageSetJSON({
+                key: local_mod_save_key,
+                value: $.toJSON(data),
+                success: success,
+                error: saveToServer
+            });
+        } else {
+            saveToServer();
+        }
+    }
 }
