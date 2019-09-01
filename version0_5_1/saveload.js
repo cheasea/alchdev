6f359b2631df1742b3f340155a0c2e9a72a7755a
Denys Denysuik <denysuikdenys@example.com>
2019-09-01 14:13:31 +0300

    saveload fixed

--- version0_5_1/saveload.js
+++ version0_5_1/saveload.js
@@ -1,54 +1,28 @@
-function save(to) {
-    var elements = [];
-    $('.element').filter(':not(:data(isDead,1))').each(function () {
-        elements.push({
-            'name': $(this).data('elementName'),
-            'offset': $(this).offset()
-        });
-    });
-    var data = {
-        "opened": opened,
-        "recipes": recipes,
-        "elements": elements,
-        "counters": counters
-    };
-
-    var success = function () {
-        $('#save').fadeOut();
-    };
-    var error = function () {
-        alert("Не удалось сохранить игру.");
-    };
-
-    if (use_local_storage_to_save) {
-        try {
-            window.localStorage[local_mod_save_key] = $.toJSON(data);
-            success();
-        } catch (e) {
-            error();
-        }
-    } else {
-        var saveToServer = function () {
-            $.post(to, {
-                "data": $.toJSON(data)
-            }, function (data) {
-                if (data == 'OK') {
-                    success();
-                } else {
-                    error();
-                }
-            });
-        };
-
-        if (typeof (VK_api) != 'undefined') {
-            VK_StorageSetJSON({
-                key: local_mod_save_key,
-                value: $.toJSON(data),
-                success: success,
-                error: saveToServer
-            });
-        } else {
-            saveToServer();
-        }
-    }
+function getModId() {
+    var regex = /[^\d]*(\d+)[^\d]*/gm;
+    var res = regex.exec($('#load')[0].onclick.toString());
+    return res[1];
 }
+
+$('#save a')[0].onclick = () => {
+    $('#save').append('<span id="save_msg">(игра сохраняется)</span>');
+    stopGame();
+
+    setTimeout(() => {
+        runGame();
+        save('/versions/' + getModId() + '/save/');
+        $('#save_msg').remove();
+    }, 500);
+}
+
+function stopGame() {
+    $('.element').draggable('disable');
+    $('.element').droppable('disable');
+    $('.element').selectable('disable');
+}
+
+function runGame() {
+    $('.element').draggable('enable');
+    $('.element').droppable('enable');
+    $('.element').selectable('enable');
+} 
