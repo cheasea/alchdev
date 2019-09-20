02d65660caa9869dc0af2eab7cd1145e0ae96ac2
Denys Denysuik <denysuikdenys@example.com>
2019-09-20 20:01:56 +0300

    version to beta test

--- versionControl.js
+++ versionControl.js
@@ -1,7 +1,7 @@
 function versionCheck() {
     if (!settings.version) return;
     var updates = {
-        "0.5.2": "version0_5_2.js"
+        "b0.5.2": "version0_5_2.js"
     }
 
     var scripts = updates[settings.version];
