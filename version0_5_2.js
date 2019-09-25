7e17293b4504577e789ee1b11267513c56f4c550
Dan1LExtr1m <43620010+Dan1LExtr1m@users.noreply.github.com>
2019-09-25 10:41:31 +0300

    Удаление элементов исправленно

--- version0_5_2.js
+++ version0_5_2.js
@@ -323,17 +323,23 @@ function onSelectStop() {
     refreshHint();
     updateCounters();
 }
-
 function destroyElement(element, anim = true) {
-    element = element.filter('.element').not('.static'); //filter unkillable statics
-    element.data("isDead", 1);
+    element = element.filter('.element').not('.static').not(':data(isDeleting, 1)'); 
+
+    if (!element[0]) return;
+    
+    element.data('isDeleting', 1)
+    element.data('isDead', 1);
     element.draggable('disable');
     element.droppable('disable');
-    //element.effect(destroy_effects[Math.floor(Math.random()*destroy_effects.length)],{},1000, function(){element.remove();})
-    if (anim) element.fadeOut(1000, function () {
+
+    if (anim) {
+        element.fadeOut(1000, function() {
+            element.remove();
+        });
+    } else {
         element.remove();
-    });
-    else element.remove();
+    }
 }
 
 function getModId() {
