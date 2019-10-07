e44bc294b1ea57d301104072cdb38b294539c661
Dan1LExtr1m <43620010+Dan1LExtr1m@users.noreply.github.com>
2019-10-08 00:09:08 +0300

    Перемещён код

--- version0_6.js
+++ version0_6.js
@@ -17,6 +17,108 @@ let board = document.getElementById('board');
 let allElements = {};
 let allReactions = {};
 
+class Element {
+    constructor(str) {
+        this.name = str; // название, к которому обращаются
+        this.cleanName = name.replace(/\[.+\]$/, ''); // название, которое видит пользователь
+
+        this.class = classes[this.name] || '';
+        
+        if (settings.images) {
+            this.image = labels[this.name];
+        }
+
+        this.html = [];
+    }
+
+    add(x, y) {
+        let elem = document.createElement('div');
+
+        /* CSS-properties */
+        elem.style.transition = 'opacity 1s, translate 1s';
+        elem.style.opacity = 0;
+        elem.style.borderRadius = '10px';
+        elem.style.zIndex = '1';
+
+        /* Custom properties */
+        elem.style.position = 'abstract';
+        elem.style.top = `${y}px`
+        elem.style.left = `${x}px`;
+
+        elem.classList = 'element ' + this.class;
+
+        if (this.image) {
+            let img = document.createElement('img');
+
+            img.src = MEDIA_URL + this.image;
+
+            elem.appendChild(img);
+            elem.classList += ' element-icon img-element';
+        } else {
+            elem.innerHTML = this.name;
+        }
+
+        $(elem).data('elementName', name);
+        this.html.push(elem);
+
+        board.appendChild(elem);
+
+        $(elem).draggable({
+            scroll: false,
+            start: function() {
+                let value = $('.element').length + 1;
+
+                $(this).stop();
+                $(this).css('z-index', value);
+                $(this)[0].style.transition = 'opacity 1s';
+            }
+        });
+
+        $(elem).droppable({
+            accept: '.element:not(:data(isDead, 1))',
+            drop: onDrop
+        });
+
+        elem.style.opacity = 1;
+    }
+
+    delete() {
+        let elem = this.html[0];
+
+        if (!elem) return;
+
+        deleteElement(elem);
+    }
+
+    move(to) {
+        let elem = this.html[0];
+
+        if (!elem) return;
+
+        elem.style.transition = 'opacity 1s, top 1s, left 1s';
+
+        elem.addEventListener('transitionend', function () {
+            elem.style.transition = 'opacity 1s';
+        });
+
+        elem.style.top = `${to.y}px`;
+        elem.style.left = `${to.x}px`;
+    }
+}
+
+class Reaction {
+    constructor(reagents, products) {
+        this.reagents = reagents.split('+');
+        this.result = this.products;
+    }
+
+    run(pos) {
+        this.result.forEach(item => {
+            allElements[item].add(pos);
+        });
+    }
+}
+
 for (let elem of inits) {
     countElements(elem);
 }
@@ -161,106 +263,4 @@ function onSelectStop() {
 
     refreshHint();
     updateCounters();
-}
-
-class Element {
-    constructor(str) {
-        this.name = str; // название, к которому обращаются
-        this.cleanName = name.replace(/\[.+\]$/, ''); // название, которое видит пользователь
-
-        this.class = classes[this.name] || '';
-        
-        if (settings.images) {
-            this.image = labels[this.name];
-        }
-
-        this.html = [];
-    }
-
-    add(x, y) {
-        let elem = document.createElement('div');
-
-        /* CSS-properties */
-        elem.style.transition = 'opacity 1s, translate 1s';
-        elem.style.opacity = 0;
-        elem.style.borderRadius = '10px';
-        elem.style.zIndex = '1';
-
-        /* Custom properties */
-        elem.style.position = 'abstract';
-        elem.style.top = `${y}px`
-        elem.style.left = `${x}px`;
-
-        elem.classList = 'element ' + this.class;
-
-        if (this.image) {
-            let img = document.createElement('img');
-
-            img.src = MEDIA_URL + this.image;
-
-            elem.appendChild(img);
-            elem.classList += ' element-icon img-element';
-        } else {
-            elem.innerHTML = this.name;
-        }
-
-        $(elem).data('elementName', name);
-        this.html.push(elem);
-
-        board.appendChild(elem);
-
-        $(elem).draggable({
-            scroll: false,
-            start: function() {
-                let value = $('.element').length + 1;
-
-                $(this).stop();
-                $(this).css('z-index', value);
-                $(this)[0].style.transition = 'opacity 1s';
-            }
-        });
-
-        $(elem).droppable({
-            accept: '.element:not(:data(isDead, 1))',
-            drop: onDrop
-        });
-
-        elem.style.opacity = 1;
-    }
-
-    delete() {
-        let elem = this.html[0];
-
-        if (!elem) return;
-
-        deleteElement(elem);
-    }
-
-    move(to) {
-        let elem = this.html[0];
-
-        if (!elem) return;
-
-        elem.style.transition = 'opacity 1s, top 1s, left 1s';
-
-        elem.addEventListener('transitionend', function () {
-            elem.style.transition = 'opacity 1s';
-        });
-
-        elem.style.top = `${to.y}px`;
-        elem.style.left = `${to.x}px`;
-    }
-}
-
-class Reaction {
-    constructor(reagents, products) {
-        this.reagents = reagents.split('+');
-        this.result = this.products;
-    }
-
-    run(pos) {
-        this.result.forEach(item => {
-            allElements[item].add(pos);
-        });
-    }
 }
