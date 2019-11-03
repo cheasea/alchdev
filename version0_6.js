377363c4c77d4d8877ee17e95811206142bf2b19
Dan1LExtr1m <43620010+Dan1LExtr1m@users.noreply.github.com>
2019-11-03 23:26:47 +0300

    Двоеточия необязательны

--- version0_6.js
+++ version0_6.js
@@ -476,9 +476,9 @@ function react(r, b = false) {
                     counter.forEach(item => {
                         let isName = item.match(/set (.+)/);
                         let isValue = item.match(/\s+?([+|\-|=])(\d+(?:\.\d+)?)/);
-                        let isMin = item.match(/\s+?min:\s+?([+|\-]?\d+(?:\.\d+)?)\s+?{(.+)}/);
-                        let isMax = item.match(/\s+?max:\s+?([+|\-]?\d+(?:\.\d+)?)\s+?{(.+)}/);
-                        let isAt = item.match(/\s+?at:\s+?([+|\-]?\d+(?:\.\d+)?)\s+?{(.+)}/);
+                        let isMin = item.match(/\s+?min:?\s+?([+|\-]?\d+(?:\.\d+)?)\s+?{(.+)}/);
+                        let isMax = item.match(/\s+?max:?\s+?([+|\-]?\d+(?:\.\d+)?)\s+?{(.+)}/);
+                        let isAt = item.match(/\s+?at:?\s+?([+|\-]?\d+(?:\.\d+)?)\s+?{(.+)}/);
 
                         if (isName) name = isName[1];
 
