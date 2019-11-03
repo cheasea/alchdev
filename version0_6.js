9f721be10dd093b8352921511ac30aa81575cdf2
Dan1LExtr1m <43620010+Dan1LExtr1m@users.noreply.github.com>
2019-11-03 21:48:32 +0300

    Регулярные выражения исправлены

--- version0_6.js
+++ version0_6.js
@@ -476,8 +476,8 @@ function react(r, b = false) {
                     counter.forEach(item => {
                         let isName = item.match(/set (.+)/);
                         let isValue = item.match(/\s+?([+|\-|=])(\d+(?:\.\d+)?)/);
-                        let isMin = item.match(/\s+?min:\s+?([+|\-]\d+(?:\.\d+)?)\s+?{(.+)}/);
-                        let isMax = item.match(/\s+?max:\s+?([+|\-]\d+(?:\.\d+)?)\s+?{(.+)}/);
+                        let isMin = item.match(/\s+?min:\s+?([+|\-]?\d+(?:\.\d+)?)\s+?{(.+)}/);
+                        let isMax = item.match(/\s+?max:\s+?([+|\-]?\d+(?:\.\d+)?)\s+?{(.+)}/);
 
                         if (isName) name = isName[1];
 
