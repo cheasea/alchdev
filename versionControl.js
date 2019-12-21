ffebef9fd575afe986479941069c3066de03984d
Denys Denysuik <denysuikdenys@example.com>
2019-12-21 15:44:44 +0200

    vc change

--- versionControl.js
+++ versionControl.js
@@ -20,7 +20,11 @@ function versionCheck() {
 
   let updates = {
     "0.5.2": ["version0_5_2.js"],
-    "b0.6": ["counterParser.js", "version0_6.js"]
+    "b0.6": [
+      "counterParser.js",
+      "version0_6/conditions.js",
+      "version0_6/alch.js"
+    ]
   };
 
   let scripts = updates[settings.version];
