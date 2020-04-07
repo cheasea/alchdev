81c0ead5fb89c3b5af2e3491fb379e5b882c6627
Denys Denysuik <denysuikdenys@gmail.com>
2020-04-07 23:27:47 +0300

    borders fix

--- versionControl.js
+++ versionControl.js
@@ -1,11 +1,11 @@
-let githubRepLink = "https://cheasea.github.io/alchdev/";
+let githubRepLink = 'https://cheasea.github.io/alchdev/';
 
 function loadScripts(scripts) {
   if (!scripts) return;
   if (scripts.length === 0) return;
 
   let link = scripts.shift();
-  let script = document.createElement("script");
+  let script = document.createElement('script');
   script.src = githubRepLink + link;
   script.defer = true;
   document.head.appendChild(script);
@@ -18,17 +18,18 @@ function loadScripts(scripts) {
 function versionCheck() {
   if (!settings.version) return;
 
-  if (settings.version === "last") settings.version = "0.5.2";
+  if (settings.version === 'last') settings.version = '0.5.2';
 
   let updates = {
-    "0.5.2": ["version0_5_2.js"],
-    "0.6": [
-      "velocity.js",
-      "selection.js",
-      "counterParser.js",
-      "version0_6/conditions.js",
-      "version0_6/alch.js",
-      "version0_6/selectable.js"
+    '0.5.2': ['version0_5_2.js'],
+    '0.6': [
+      'velocity.js',
+      'selection.js',
+      'counterParser.js',
+      'version0_6/conditions.js',
+      'version0_6/alch.js',
+      'version0_6/selectable.js',
+      'version0_6/borders.css',
     ],
   };
 
@@ -38,15 +39,15 @@ function versionCheck() {
 }
 
 versionCheck();
-$("title").text(
-  $("title").text().replace("Алхимия 0.5 beta", "Алхимия 0.6 beta")
+$('title').text(
+  $('title').text().replace('Алхимия 0.5 beta', 'Алхимия 0.6 beta')
 );
 
 let winterLogo = false;
 
 if (winterLogo) {
-  $("#ingamelogo").css(
-    "background",
+  $('#ingamelogo').css(
+    'background',
     `url('${githubRepLink}winterlogo.png') fixed left top no-repeat`
   );
 }
