27e28aeb6530de389426402d0ee222ac5db86405
Denys Denysuik <denysuikdenys@gmail.com>
2020-04-07 23:15:34 +0300

    fix elements in top list

--- version0_6/selectable.js
+++ version0_6/selectable.js
@@ -1,42 +1,42 @@
 const selection = new Selection({
-  class: "ui-selectable-helper",
+  class: 'ui-selectable-helper',
   frame: document,
   startThreshold: 0,
-  tapMode: "touch",
-  selectables: [".element"],
-  startareas: ["body"],
-  boundaries: ["body"],
-  selectionAreaContainer: "body",
+  tapMode: 'touch',
+  selectables: ['#board .element'],
+  startareas: ['body'],
+  boundaries: ['body'],
+  selectionAreaContainer: 'body',
 });
 
 selection
-  .on("beforestart", (evt) => {
+  .on('beforestart', (evt) => {
     return !evt.oe.composedPath().some((item) => {
-      if (item.classList) return item.classList.contains("element");
+      if (item.classList) return item.classList.contains('element');
     });
   })
-  .on("move", (evt) => {
+  .on('move', (evt) => {
     let added = evt.changed.added;
     let removed = evt.changed.removed;
 
     if (added.length > 0) {
       added.forEach((item) => {
-        item.className += " selected";
+        item.className += ' selected';
       });
     }
 
     if (removed.length > 0) {
       removed.forEach((item) => {
-        item.className = item.className.replace(" selected", "");
+        item.className = item.className.replace(' selected', '');
       });
     }
   })
-  .on("stop", (evt) => {
+  .on('stop', (evt) => {
     let selected = evt.selected;
 
     if (selected.length > 0) {
       selected.forEach((item) => {
-        item.className = item.className.replace(" selected", "");
+        item.className = item.className.replace(' selected', '');
       });
 
       onSelectStop(selected);
